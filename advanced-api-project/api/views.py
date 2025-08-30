22dff8a3528c298eab425bf4a7bbb42ccde3cff2
Kayode Thomas <thomaskayode8@gmail.com>
2025-08-30 12:30:58 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -33,6 +33,9 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
 
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
