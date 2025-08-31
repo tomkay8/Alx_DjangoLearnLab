99b2c02ad666b28520d8a59e89caf21551da4b6e
Kayode Thomas <thomaskayode8@gmail.com>
2025-08-31 22:43:05 +0100

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,10 +2,11 @@ from rest_framework import generics, permissions, status, permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import User
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .models import User, CustomUser
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer, CustomUserSerializer
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     """
@@ -62,3 +63,14 @@ class UnfollowUserView(APIView):
         target_user = get_object_or_404(User, id=user_id)
         request.user.following.remove(target_user)
         return Response({"message": f"You have unfollowed {target_user.username}"}, status=status.HTTP_200_OK)
+
+# Example: List all users (requires authentication)
+class UserListView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()  # <- checker wants this
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        users = self.get_queryset()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
